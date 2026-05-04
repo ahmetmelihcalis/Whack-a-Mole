a11ecdc6023568258df19842441aca2482dc98a7
A. Melih Çalış <sengmelihcalis@gmail.com>
2026-05-04 23:35:32 +0300

    Temel oyun penceresi, SDL2 kütüphanesi ve CMakeLists konfigürasyonu eklendi

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Whack-a-Mole",
+        SDL_WINDOWPOS_CENTERED, // X ekseninde ortalama
+        SDL_WINDOWPOS_CENTERED, // Y ekseninde ortalama
+        800,                    // Genişlik
+        600,                    // Yükseklik
+        SDL_WINDOW_SHOWN        // Ekranda gösterme
+    );
+
+    if (window == nullptr)
+    {
+        SDL_Quit();
+        return -1;
+    }
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                isRunning = false;
+            }
+        }
+
+        SDL_Delay(16); // 60 FPS sabitleme için bekleme
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
